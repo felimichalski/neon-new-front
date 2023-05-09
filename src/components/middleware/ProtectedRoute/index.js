4cd5274456b486bb98dc1f1d4e8f5825e7465688
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-09 17:17:58 -0300

    feature - Protected route

---
+++ src/components/middleware/ProtectedRoute/index.js
@@ -0,0 +1,21 @@
+import { Navigate, Outlet } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { getUserDetails } from "../../../features/actions/authActions"
+/* import { useAuth0 } from "@auth0/auth0-react"; */
+
+function ProtectedRoute() {
+  /* const { isAuthenticated, user } = useAuth0(); */
+  /* const { logedUser } = useSelector((state) => state.user); */
+  
+  /* let hola = authInfo.then((res)=>{ testValue = res.payload;console.log("res.payload",res.payload)}).catch(e=>console.log(e)) */
+  
+  const logedUser = useSelector(state => state.auth.userInfo)
+  console.log(logedUser)
+
+
+  return logedUser?.admin ? <Outlet /> : <Navigate to={"/"} />;
+}
+
+export default ProtectedRoute;
