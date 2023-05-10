2bbac6c070c16ef02664bdc4936d7be330e94fe2
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-10 14:23:58 -0300

    feature - product slice

---
+++ src/features/slices/productSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import { postProduct } from '../actions/productActions';
+
+const initialState = {
+            image: "",
+            category:"",
+            description:"",
+            isFeatured: true,
+            title:"",
+            unit_price:0
+}
+
+const productSlice = createSlice({
+    name:"product",
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [postProduct.pending]: (state) => {
+            state.error = null;
+            state.status = 'pending';
+          },
+          [postProduct.fulfilled]: (state, { payload }) => {
+            state.success = true; // post successful
+            state.status = 'success';
+          },
+          [postProduct.rejected]: (state, { payload }) => {
+            state.error = payload;
+            state.status = 'rejected';
+          },
+    }
+})  
+
+export default productSlice.reducer;
