d7b96132b3c15a807adad8c38beaa1beb321be3e
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-10 14:23:10 -0300

    feature - product reducer

--- src/app/store.js
+++ src/app/store.js
@@ -4,18 +4,22 @@ import featuredReducer from '../features/slices/featuredSlice'
 import cartReducer, { getTotals } from '../features/slices/cartSlice'
 import authReducer from '../features/slices/authSlice'
 import socialReducer from '../features/slices/socialSlice'
+import productReducer from "../features/slices/productSlice"
 import { socialFetch } from '../features/actions/socialActions'
 import { getUserDetails } from '../features/actions/authActions'
+import { postProduct } from '../features/actions/productActions'
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
         featured: featuredReducer,
         social: socialReducer,
-        auth: authReducer
+        auth: authReducer,
+        product: productReducer
     }
 });
 
 store.dispatch(socialFetch());
 store.dispatch(getUserDetails());
 store.dispatch(getTotals());
+store.dispatch(postProduct());
