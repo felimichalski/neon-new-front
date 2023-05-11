41765bdf37a66365232c7050ab95081deb055c94
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-11 18:50:48 -0300

    feature - adding public reducer

--- src/app/store.js
+++ src/app/store.js
@@ -5,9 +5,11 @@ import cartReducer, { getTotals } from '../features/slices/cartSlice'
 import authReducer from '../features/slices/authSlice'
 import socialReducer from '../features/slices/socialSlice'
 import productReducer from "../features/slices/productSlice"
+import publicReducer from "../features/slices/publicSlice"
 import { socialFetch } from '../features/actions/socialActions'
 import { getUserDetails } from '../features/actions/authActions'
 import { postProduct } from '../features/actions/productActions'
+import { getPublics } from '../features/actions/publicActions'
 
 export const store = configureStore({
     reducer: {
@@ -15,7 +17,8 @@ export const store = configureStore({
         featured: featuredReducer,
         social: socialReducer,
         auth: authReducer,
-        product: productReducer
+        product: productReducer,
+        public: publicReducer,
     }
 });
 
@@ -23,3 +26,4 @@ store.dispatch(socialFetch());
 store.dispatch(getUserDetails());
 store.dispatch(getTotals());
 store.dispatch(postProduct());
+store.dispatch(getPublics());
