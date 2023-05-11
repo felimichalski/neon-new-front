6b3f52cf78eef7934caa48e529a2fef87cca07b1
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-10 21:01:49 -0300

    feature - pagination slice

--- src/components/ProductsSection/index.js
+++ src/components/ProductsSection/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Product from '../Product'
 import { Box } from '@mantine/core'
+import CreateProduct from '../CreateProduct'
 
-const ProductsSection = ({ products }) => {
+const ProductsSection = ({ products, page }) => {
+    console.log(products.length)
+    /* let productContainer = []; */
+    /* products.map(prod=>productContainer.push(prod)) */
+    const productsContainer = products.slice(page===1?0:(page-1)*6,page*6);
     return (
         <Box style={{display: 'flex', flexWrap: 'wrap'}}>
-            {products.map((product, key) => (
+            {productsContainer.map((product, key) => (
                 <Box style={{
                     width: '33%',
                     padding: '1rem'
