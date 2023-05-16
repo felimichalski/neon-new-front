8c6c6f6c437accf136213f2ce21185a212b2779c
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-16 17:48:18 -0300

    feature - main title edit

--- src/components/TitleBox/index.js
+++ src/components/TitleBox/index.js
@@ -1,8 +1,13 @@
 import { Carousel } from '@mantine/carousel';
-import { BackgroundImage, createStyles, Title, Box } from '@mantine/core'
+import { BackgroundImage, createStyles, Title, Box, Button, Input, TextInput } from '@mantine/core'
 import { getPublics } from '../../features/actions/publicActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect} from 'react';
+import { Edit2Outline } from "@styled-icons/evaicons-outline/Edit2Outline"
+import { Tick } from "@styled-icons/typicons/Tick"
+import { Cross } from "@styled-icons/entypo/Cross"
+import { useForm } from '@mantine/form';
+import { updatePublic } from '../../features/actions/publicActions';
 
 import background1 from '../../assets/1.webp'
 import background2 from '../../assets/bgHome.webp'
@@ -10,7 +15,7 @@ import background3 from '../../assets/bgHome2.jpeg'
 import { useRef } from 'react';
 import Autoplay from 'embla-carousel-autoplay';
 
-const useStyles = createStyles((theme) => ({
+const useStyles = createStyles((theme, { hoverEffects }) => ({
     root: {
         height: '70vh',
         width: '100%',
@@ -32,23 +37,103 @@ const useStyles = createStyles((theme) => ({
     title: {
         zIndex: 2,
         color: theme.white
+    },
+    titleBox: {
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        color:"white",
+        position:"absolute",
+        top:"50%",
+        left:"50%", 
+        margin:"auto", 
+        zIndex:"1000",
+        transform: "translate(-50%, -50%)"
+    },
+    editButton: {
+        background:"none",
+        transition:"transform 0.2s",
+        padding:"0",
+        margin:"0 0.5rem",
+        [`&:hover`]: {
+            transform: !hoverEffects && "scale(1.1)",
+            background: !hoverEffects && "none"
+        }
+    },
+    titleInput:{
+        /* background:"none !important",
+        border:"none ",
+        borderBottom:"1px solid white", */
+    },
+    form: {
+        display:"flex",
+        flexDirection:"row"
     }
 }))
 
-const TitleBox = () => {
-    useEffect(() => {
-      dispatch(getPublics())
-    }, [])
+const TitleBox = ({ hoverEffects, updateText, setUpdateText }) => {
+    
     
     const publics = useSelector(state => state.public.items)
+    const logedUser = useSelector(state=> state.auth.userInfo)
     const dispatch = useDispatch()
-    const { classes } = useStyles();
+    const { classes } = useStyles({ hoverEffects });
     const autoplay = useRef(Autoplay({ delay: 5000, stopOnInteraction: false }))
-    console.log(publics)
+    const firstPublic = publics.length?publics.filter(title => title.name==="Home Title")[0].content:""
+    const form = useForm({
+        initialValues:{
+            content:"",
+            name:"Home Title"
+        },
+        validate: {
+            content: (value) => (value === "" ? 'Debes escribir algo para cambiar este texto' : null),
+        }
+    })
+
+    useEffect(() => {
+        dispatch(getPublics())
+      }, [dispatch,publics[0]])
+
+    const handleEdit = (e)=>{
+        console.log(e.currentTarget.name)
+        if(e.currentTarget.name==="edit"){
+            setUpdateText("HomeTitle")
+        }else if(e.currentTarget.name==="ok"){
+            dispatch(getPublics)
+        }else{
+            dispatch(getPublics)
+            setUpdateText("")
+        }
+    }
 
     return (
         <Box sx={{position:"relative"}}>
-        <Title sx={{color:"white",position:"absolute",top:"50%",left:"50%", margin:"auto", zIndex:"1000",transform: "translate(-50%, -50%)"}}>Tus neones favoritos</Title>
+            <Box className={classes.titleBox}>
+
+                {updateText!=="HomeTitle"?
+                <Title>{firstPublic}</Title>:
+                <form className={classes.form} onSubmit={form.onSubmit(values => dispatch(updatePublic(values)))}>
+                    <TextInput
+                    placeholder={firstPublic}
+                    className={classes.titleInput}
+                    {...form.getInputProps("content")}
+                    />
+                    <Button type="submit" name="ok" onClick={(e)=>handleEdit(e)} className={classes.editButton}><Tick size={27}/></Button>
+                    <Button name="back" onClick={(e)=>handleEdit(e)} className={classes.editButton}><Cross size={27}/></Button>
+                </form>}
+
+                {logedUser?.admin && updateText!=="HomeTitle"?
+                <Button name="edit" onClick={(e)=>handleEdit(e)} className={classes.editButton}><Edit2Outline size={27}/></Button>:
+                ""}
+
+                {/* {updateText==="title"?
+                <Box>
+                    <Button name="ok" onClick={(e)=>handleEdit(e)} className={classes.editButton}><Tick size={27}/></Button>
+                    <Button name="back" onClick={(e)=>handleEdit(e)} className={classes.editButton}><Cross size={27}/></Button>
+                </Box>:
+                ""
+                } */}
+            </Box>
         <Carousel
             slideSize='100%'
             loop
