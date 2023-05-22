b8bf9d365361a72e5b4149ba2a1ae1d6d8c43347
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-22 15:37:58 -0300

    feature - creating ProductDetail

---
+++ src/components/ProductDetail/index.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react"
+
+const ProductDetail = ()=>{
+
+   useEffect(() => {
+
+   }, [])
+   
+ return(
+    <div>
+      Hello world (Detail)
+    </div>
+ )   
+}
+export default ProductDetail
