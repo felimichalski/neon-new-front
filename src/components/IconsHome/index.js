a36f0d88f7cf13a7591ae92b3c2bcbc932531d71
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-22 15:37:37 -0300

    feature - creating IconsHome

---
+++ src/components/IconsHome/index.js
@@ -0,0 +1,45 @@
+import { Box, createStyles, Flex, Container, Text, Image } from "@mantine/core"
+import { Truck } from "@styled-icons/bootstrap/Truck"
+import { MoneyHand } from "@styled-icons/fluentui-system-regular/MoneyHand"
+import { CheckShield } from "@styled-icons/boxicons-regular/CheckShield"
+import { Payment } from "@styled-icons/fluentui-system-regular/Payment"
+
+const useStyles = createStyles((theme) => ({
+flexContainer:{
+    alignItems:"center",
+    justifyContent:"space-evenly"
+},
+iconContainer:{
+    display:"flex",
+    width:"25%",
+    flexDirection:"column",
+    alignItems:"center",
+    justifyContent:"center",
+   /*  background:"grey", */
+    fontFamily:'ITC Avant Garde Gothic',
+    textAlign:"center",
+}
+}))
+
+const IconsHome = () =>{
+    const {classes} = useStyles()
+
+    return(
+        <Flex  className={classes.flexContainer}>
+            <Box className={classes.iconContainer}>
+                <Truck size={100}/>
+                <Text>Contamos con envíos a todo el país</Text>
+            </Box>
+            <Box className={classes.iconContainer}>
+                <Payment size={100}/>
+                <Text>Pagá utilizando cualquier medio de pago</Text>
+                
+            </Box>
+            <Box className={classes.iconContainer}>
+                <Image height={100} width={100} src="https://rankea.com.ar/wp-content/uploads/2019/05/pago-mercado.png" alt="mp"/>
+                <Text>Tu compra protegida por MercadoPago</Text>
+            </Box>
+        </Flex>
+    )
+}
+export default IconsHome
