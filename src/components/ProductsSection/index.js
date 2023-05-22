22b47ec56df56d93d86e4f1fc98f3167a83f35a9
SebastianCalabro <sebastianjoaquincalabro@gmail.com>
2023-05-22 15:38:09 -0300

    cleaning logs

--- src/components/ProductsSection/index.js
+++ src/components/ProductsSection/index.js
@@ -4,7 +4,6 @@ import { Box } from '@mantine/core'
 import CreateProduct from '../CreateProduct'
 
 const ProductsSection = ({ products, page }) => {
-    console.log(products.length)
     /* let productContainer = []; */
     /* products.map(prod=>productContainer.push(prod)) */
     const productsContainer = products.slice(page===1?0:(page-1)*6,page*6);
